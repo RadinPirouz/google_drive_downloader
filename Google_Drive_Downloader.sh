0656e3d44db130d87f293aca7435072a948ce5db
RadinPirouz <75082987+RadinPirouz@users.noreply.github.com>
2023-07-02 10:20:10 +0330

    Update Google_Drive_Downloader.sh

--- Google_Drive_Downloader.sh
+++ Google_Drive_Downloader.sh
@@ -5,8 +5,6 @@ echo "Enter Google Drive Link: "
 read link
 echo "Enter File Name: "
 read filename
-test="test"
 fileid=`echo $link | awk  'gsub("&export=download", "", $0)' | grep -Po 'id=\K[^"]*'`
 final_link="https://drive.google.com/uc?id=${fileid}&export=download&confirm=yes"
-echo $final_link
 curl -o ${filename} -L $final_link
