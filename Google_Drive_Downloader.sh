2600a0c95b449d17084b7f5fdf6b7f2610a0d4d4
RadinPirouz <75082987+RadinPirouz@users.noreply.github.com>
2023-12-20 21:05:19 +0330

    Update Google_Drive_Downloader.sh

--- Google_Drive_Downloader.sh
+++ Google_Drive_Downloader.sh
@@ -1,10 +1,40 @@
 #!/bin/bash
 
+# Function to extract file ID from Google Drive link
+get_file_id() {
+    echo $1 | awk 'gsub("&export=download", "", $0)' | grep -Po 'id=\K[^"&?]*'
+}
 
 echo "Enter Google Drive Link: "
 read link
+
+# Validate that a link has been provided
+if [ -z "$link" ]; then
+    echo "Error: Please enter a Google Drive link."
+    exit 1
+fi
+
 echo "Enter File Name: "
 read filename
-fileid=`echo $link | awk  'gsub("&export=download", "", $0)' | grep -Po 'id=\K[^"]*'`
+
+# Extract file ID from the link
+fileid=$(get_file_id "$link")
+
+# Validate that the file ID was successfully extracted
+if [ -z "$fileid" ]; then
+    echo "Error: Unable to extract file ID from the provided link."
+    exit 1
+fi
+
+# Construct the final download link
 final_link="https://drive.google.com/uc?id=${fileid}&export=download&confirm=yes"
-curl -o ${filename} -L $final_link
+
+# Download the file using curl
+curl -o "$filename" -L "$final_link"
+
+# Check if the download was successful
+if [ $? -eq 0 ]; then
+    echo "Download complete. File saved as: $filename"
+else
+    echo "Error: Unable to download the file."
+fi
